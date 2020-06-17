025db7ef6a5e49bf19314181852781af349dd211
mano92fuentesjimenez <m92fuentes@gmail.com>
2020-06-16 20:48:29 -0400

    initial commit

---
+++ src/services/store/index.js
@@ -0,0 +1,20 @@
+import { applyMiddleware, combineReducers, compose, createStore } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import reducers from './reducers';
+import sagas from './sagas';
+
+const composeEnhancers = typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+  ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({ })
+  : compose;
+
+const sagaMiddleware = createSagaMiddleware();
+const enhancer = composeEnhancers(applyMiddleware(sagaMiddleware));
+const enhancedReducers = combineReducers({
+  ...reducers,
+});
+const store = createStore(enhancedReducers, enhancer);
+sagas.forEach(saga => {
+  sagaMiddleware.run(saga)
+});
+
+export default store;
