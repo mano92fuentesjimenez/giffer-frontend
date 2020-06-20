2a964118459e17458826eaa4528a60f1db556d63
mano92fuentesjimenez <m92fuentes@gmail.com>
2020-06-19 22:01:28 -0400

    fix date valid checking

--- src/components/GifMetadata/GifMetadata.js
+++ src/components/GifMetadata/GifMetadata.js
@@ -3,6 +3,7 @@ import bem from 'bem-cn';
 import Rating from 'components/Rating/Rating';
 import { FormattedDate, FormattedMessage } from 'react-intl';
 import './GifMetadata.scss';
+import isValidDate from 'helpers/isValidDate';
 
 const b = bem('components-gif-metadata');
 const GifMetadata = ({ gif }) => {
@@ -13,6 +14,10 @@ const GifMetadata = ({ gif }) => {
     userName,
     rating,
   } } = gif;
+
+  const creationTimeDate = new Date(creationTime);
+  const trendingTimeDate = new Date(trendingTime);
+
   return (
     <div className={b()}>
       {title && <div className={b('row')()}>
@@ -29,14 +34,14 @@ const GifMetadata = ({ gif }) => {
         <div className={b('field')()}><h4><FormattedMessage id={'rating'}/></h4></div>
         <div className={b('value')()}><Rating rating={rating}/></div>
       </div>
-      { creationTime && <div className={b('row')()}>
+      { creationTime && isValidDate(creationTimeDate) && <div className={b('row')()}>
           <div className={b('field')()}><h4><FormattedMessage id={'creationTime'}/></h4></div>
-          <div className={b('value')()}><FormattedDate value={new Date(creationTime)}/></div>
+          <div className={b('value')()}><FormattedDate value={creationTimeDate}/></div>
         </div>
       }
-      { trendingTime && <div className={b('row')()}>
+      { trendingTime && isValidDate(trendingTimeDate) && <div className={b('row')()}>
           <div className={b('field')()}><h4><FormattedMessage id={'trendingTime'}/></h4></div>
-          <div className={b('value')()}><FormattedDate value={new Date(trendingTime)}/></div>
+          <div className={b('value')()}><FormattedDate value={trendingTimeDate}/></div>
         </div>
       }
     </div>
