022d4fcc3fccd365c5a9317efde7ac0a3c3b2adb
mano92fuentesjimenez <m92fuentes@gmail.com>
2020-06-24 05:56:34 -0400

    fix responsive settings scene

--- src/scenes/UserSettings/components/UserSettings.js
+++ src/scenes/UserSettings/components/UserSettings.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import bem from 'bem-cn'
 import './UserSettings.scss';
 import { FormattedMessage } from 'react-intl';
@@ -6,17 +6,39 @@ import PersonalData from 'scenes/UserSettings/components/PersonalData';
 import ContentRating from 'scenes/UserSettings/components/ContentRating';
 
 const b = bem('scenes-user-settings');
+const tabs = [
+  {
+    messageId: 'settings_personal_title',
+    component: PersonalData,
+  },
+  {
+    messageId: 'content_rating_settings',
+    component: ContentRating,
+  }
+]
 const UserSettings = () => {
 
+  const [openedTab, setOpenedTab] = useState(0);
+
+  const OpenedTabComponent = tabs[openedTab].component;
+  const onTabClick = (tabPos) => () => setOpenedTab(tabPos);
   return (
     <div className={b()}>
-      <div className={b('title')()}><FormattedMessage id={'settings'}/> </div>
-      <div className={b('settings-container')()}>
-        <PersonalData/>
-        <div className={b('separator')()}/>
-        <div className="ml-4 flex-grow-1 d-flex">
-          <ContentRating/>
-        </div>
+      <div className={b('title')()}><FormattedMessage id={'settings'}/></div>
+      <div className={b('tabs-container')()}>
+        {tabs.map((t, index) => (
+          <div
+            key={index}
+            className={b('tab')({ selected: openedTab === index })()}
+            onClick={onTabClick(index)}
+          >
+            <FormattedMessage id={t.messageId}/>
+          </div>
+        ))}
+      </div>
+      <div className={b('separator')()}/>
+      <div className={b('tab-container')()}>
+        <OpenedTabComponent/>
       </div>
     </div>
   )
